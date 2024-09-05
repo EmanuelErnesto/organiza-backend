37eddd1c7215cd513732362ce00e2c975f4a1349
EmanuelErnesto <emanuel.eaerego@gmail.com>
2024-09-05 19:11:51 -0300

    feat: add user entity file

---
+++ src/main/java/com/organiza/api/modules/users/infra/database/entities/UserModel.java
@@ -0,0 +1,78 @@
+package com.organiza.api.modules.users.infra.database.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "tb_users")
+public class UserModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", nullable = false, length = 200)
+    private String name;
+
+    @Column(name = "email", nullable = false, unique = true,length = 100)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 200 )
+    private String password;
+
+    @Column(name = "balance", nullable = false, precision = 2)
+    private double balance = 0.00;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false, length = 25)
+    private Role role = Role.ROLE_CLIENT;
+
+    public enum Role {
+        ROLE_ADMIN, ROLE_CLIENT
+    }
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserModel userModel = (UserModel) o;
+        return Objects.equals(id, userModel.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "UserModel{" +
+                "id=" + id +
+                '}';
+    }
+}
