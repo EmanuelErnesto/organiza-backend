078dea04161c8dbfd772aa02e29a21d0964f38ef
EmanuelErnesto <emanuel.eaerego@gmail.com>
2024-09-05 19:16:49 -0300

    feat: add delete user service

---
+++ src/main/java/com/organiza/api/modules/users/services/DeleteUserService.java
@@ -0,0 +1,25 @@
+package com.organiza.api.modules.users.services;
+
+import com.organiza.api.exception.HttpNotFoundException;
+import com.organiza.api.modules.users.infra.database.entities.UserModel;
+import com.organiza.api.modules.users.infra.database.repositories.UserRepository;
+import com.organiza.api.shared.common.utils.consts.UserErrorConsts;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class DeleteUserService {
+
+    private final UserRepository userRepository;
+
+    public void execute(UUID id) {
+        userRepository.findById(id).orElseThrow(() ->
+                new HttpNotFoundException(UserErrorConsts.USER_NOT_FOUND));
+
+        userRepository.deleteById(id);
+
+    }
+}
