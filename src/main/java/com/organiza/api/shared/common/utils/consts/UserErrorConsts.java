dac060f6f9ea02299c5a09876750f9544f75ffee
EmanuelErnesto <emanuel.eaerego@gmail.com>
2024-09-05 19:08:29 -0300

    raw: add user error messages

---
+++ src/main/java/com/organiza/api/shared/common/utils/consts/UserErrorConsts.java
@@ -0,0 +1,9 @@
+package com.organiza.api.shared.common.utils.consts;
+
+public class UserErrorConsts {
+    public static final String EMAIL_ALREADY_USED = "Theres already a user with this same email. Please try again with other email";
+    public static final String USER_NOT_FOUND = "user not found";
+    public static final String PASSWORD_MUST_EQUAL = "password must be equal";
+    public static final String PASSWORD_DOES_NOT_MATCH = "The password does not match with the old password";
+}
+
