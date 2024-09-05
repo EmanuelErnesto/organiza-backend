d3239dc45ad89f8ab2bc45232811b20017713959
EmanuelErnesto <emanuel.eaerego@gmail.com>
2024-09-05 19:03:31 -0300

    raw: add swagger config file

---
+++ src/main/java/com/organiza/api/config/SpringDocOpenApiConfig.java
@@ -0,0 +1,41 @@
+package com.organiza.api.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocOpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("security", securityScheme()))
+                .info(
+                        new Info()
+                                .title("Rest API - Organiza")
+                                .description("API desenvolvida para a aplicação de gestão financeira denominada organiza.")
+                                .version("v1")
+                                .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0"))
+                                .contact(new Contact().name("Emanuel Ernesto").email("emanuel@enterprise.com"))
+                );
+
+
+    }
+
+    private SecurityScheme securityScheme (){
+        return new SecurityScheme()
+                .description("Insert a valid bearer token for continue")
+                .type(SecurityScheme.Type.HTTP)
+                .in(SecurityScheme.In.HEADER)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .name("security");
+    }
+
+}
