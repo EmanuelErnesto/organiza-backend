6650610faf5c2075891355e93695a5c327e7122f
EmanuelErnesto <emanuel.eaerego@gmail.com>
2024-09-05 19:17:12 -0300

    feat: add show user service

---
+++ src/main/java/com/organiza/api/modules/users/services/ShowUserService.java
@@ -0,0 +1,25 @@
+package com.organiza.api.modules.users.services;
+
+import com.organiza.api.exception.HttpNotFoundException;
+import com.organiza.api.modules.users.infra.database.entities.UserModel;
+import com.organiza.api.modules.users.infra.database.repositories.UserRepository;
+import com.organiza.api.shared.common.utils.consts.UserErrorConsts;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class ShowUserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    public UserModel execute(UUID id) {
+        return userRepository.findById(id).orElseThrow(() ->
+                new HttpNotFoundException(UserErrorConsts.USER_NOT_FOUND));
+    }
+
+}
