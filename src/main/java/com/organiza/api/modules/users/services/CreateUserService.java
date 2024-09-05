3e717802730ba7e588d940e1a84e0d6d02dd4af9
EmanuelErnesto <emanuel.eaerego@gmail.com>
2024-09-05 19:16:34 -0300

    feat: add create user service

---
+++ src/main/java/com/organiza/api/modules/users/services/CreateUserService.java
@@ -0,0 +1,31 @@
+package com.organiza.api.modules.users.services;
+
+import com.organiza.api.modules.users.infra.database.entities.UserModel;
+import com.organiza.api.modules.users.infra.database.repositories.UserRepository;
+import com.organiza.api.shared.common.utils.consts.UserErrorConsts;
+import com.organiza.api.exception.HttpBadRequestException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class CreateUserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public UserModel execute(UserModel user){
+        Optional<UserModel> userEmailExists = userRepository.findByEmail(user.getEmail());
+
+        if(userEmailExists.isPresent()) throw new HttpBadRequestException(UserErrorConsts.EMAIL_ALREADY_USED);
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+
+    }
+}
