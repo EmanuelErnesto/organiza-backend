afced0bb02ccc105173e17584995d39af05e980e
EmanuelErnesto <emanuel.eaerego@gmail.com>
2024-09-05 19:23:43 -0300

    feat: add list transaction controller

---
+++ src/main/java/com/organiza/api/modules/transactions/infra/http/controllers/ListTransactionController.java
@@ -0,0 +1,57 @@
+package com.organiza.api.modules.transactions.infra.http.controllers;
+
+
+import com.organiza.api.http.exception.ApplicationError;
+import com.organiza.api.modules.transactions.domain.dtos.FindUserTransactionsDto;
+import com.organiza.api.modules.transactions.domain.dtos.TransactionResponseDto;
+import com.organiza.api.modules.transactions.domain.dtos.mappers.TransactionMapper;
+import com.organiza.api.modules.transactions.infra.database.entities.TransactionModel;
+import com.organiza.api.modules.transactions.services.ListUserTransactionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@Tag(name = "Transaction")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/transactions")
+public class ListTransactionController {
+
+    private final ListUserTransactionService listUserTransactionService;
+
+    @Operation(summary = "List transactions", description = "endpoint that list all transactions of a user",
+    responses = {
+            @ApiResponse(responseCode = "200", description = "Transactions retrieved successfully",
+            content = @Content(mediaType = "application/json",
+                    array = @ArraySchema(schema = @Schema(implementation = TransactionResponseDto.class)
+            ))),
+            @ApiResponse(responseCode = "403", description = "User don't have permission to access this resource",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApplicationError.class))),
+            @ApiResponse(responseCode = "401", description = "Access denied",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApplicationError.class))),
+    })
+    @GetMapping("/list")
+    @PreAuthorize("hasAnyRole('ADMIN', 'CLIENT')")
+    public ResponseEntity<List<TransactionResponseDto>> execute(@Valid @RequestBody FindUserTransactionsDto body){
+
+        List<TransactionModel> transactions = listUserTransactionService.execute(UUID.fromString(body.getId()));
+
+        return ResponseEntity.ok(TransactionMapper.mappingToTransactionListResponse(transactions));
+
+    }
+
+}
