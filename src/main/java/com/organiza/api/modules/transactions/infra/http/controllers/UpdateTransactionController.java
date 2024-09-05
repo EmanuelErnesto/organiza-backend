bda14497e9962cbfc018825e887532e8e58a4853
EmanuelErnesto <emanuel.eaerego@gmail.com>
2024-09-05 19:24:24 -0300

    feat: add update transaction controller

---
+++ src/main/java/com/organiza/api/modules/transactions/infra/http/controllers/UpdateTransactionController.java
@@ -0,0 +1,60 @@
+package com.organiza.api.modules.transactions.infra.http.controllers;
+
+import com.organiza.api.http.exception.ApplicationError;
+import com.organiza.api.modules.transactions.domain.dtos.CreateTransactionDto;
+import com.organiza.api.modules.transactions.domain.dtos.TransactionResponseDto;
+import com.organiza.api.modules.transactions.domain.dtos.UpdateTransactionDto;
+import com.organiza.api.modules.transactions.domain.dtos.mappers.TransactionMapper;
+import com.organiza.api.modules.transactions.infra.database.entities.TransactionModel;
+import com.organiza.api.modules.transactions.services.UpdateTransactionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@Tag(name = "Transaction")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/transactions")
+public class UpdateTransactionController {
+
+    private final UpdateTransactionService updateTransactionService;
+
+
+    @Operation(summary = "Update one transaction", description = "endpoint that update one transaction of a existent user",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Transaction updated successfully",
+                            content = @Content(mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = TransactionResponseDto.class)))),
+                    @ApiResponse(responseCode = "403", description = "User doesn't have permission to access this resource",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApplicationError.class))),
+                    @ApiResponse(responseCode = "401", description = "Access denied",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApplicationError.class))),
+                    @ApiResponse(responseCode = "404", description = "Transaction not found",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApplicationError.class))),
+                    @ApiResponse(responseCode = "404", description = "User not found",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApplicationError.class))),
+            })
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'CLIENT')")
+    public ResponseEntity<TransactionResponseDto> execute(
+            @PathVariable("id") String id,
+            @Valid @RequestBody CreateTransactionDto body
+            ) {
+
+        TransactionModel transaction = updateTransactionService.execute(id, body);
+
+        return ResponseEntity.ok(TransactionMapper.mappingToTransactionResponse(transaction));
+
+    }
+}
