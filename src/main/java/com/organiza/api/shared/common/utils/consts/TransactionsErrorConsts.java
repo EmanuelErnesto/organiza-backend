88811878351ab0d50c018ace18ac3af8d66c982e
EmanuelErnesto <emanuel.eaerego@gmail.com>
2024-09-05 19:08:18 -0300

    raw: add transaction error messages

---
+++ src/main/java/com/organiza/api/shared/common/utils/consts/TransactionsErrorConsts.java
@@ -0,0 +1,7 @@
+package com.organiza.api.shared.common.utils.consts;
+
+public class TransactionsErrorConsts {
+
+    public static final String TRANSACTION_NOT_FOUND = "Transaction not found";
+
+}
