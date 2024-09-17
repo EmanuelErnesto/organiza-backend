dbbcee729dd5ca34b4046555372b68fd1b279df9
EmanuelErnesto <emanuel.earego@gmail.com>
2024-09-17 02:08:41 -0300

    feat: add find user id method

--- src/main/java/com/organiza/api/modules/users/infra/database/repository/UserRepository.java
+++ src/main/java/com/organiza/api/modules/users/infra/database/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.organiza.api.modules.users.infra.database.repository;
 import com.organiza.api.modules.users.infra.database.entity.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.security.core.userdetails.User;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -14,5 +15,8 @@ public interface UserRepository extends JpaRepository<UserModel, UUID> {
 
     @Query("SELECT u.role FROM UserModel u WHERE u.email = :email")
     Optional<UserModel.Role> findRoleByEmail(String email);
+
+    @Query("SELECT u.id FROM UserModel u WHERE u.email = :email")
+    Optional<UUID> findUserIdByEmail(String email);
 
 }
