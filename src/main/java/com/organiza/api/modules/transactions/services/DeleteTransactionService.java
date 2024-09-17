fddfa95b51fdba6ab03238a89292116b663d580f
EmanuelErnesto <emanuel.earego@gmail.com>
2024-09-17 02:10:48 -0300

    fix: budget down value in delete

--- src/main/java/com/organiza/api/modules/transactions/services/DeleteTransactionService.java
+++ src/main/java/com/organiza/api/modules/transactions/services/DeleteTransactionService.java
@@ -1,8 +1,11 @@
 package com.organiza.api.modules.transactions.services;
 
 import com.organiza.api.exception.HttpNotFoundException;
+import com.organiza.api.modules.budgets.infra.database.repository.BudgetRepository;
 import com.organiza.api.modules.transactions.infra.database.entity.TransactionModel;
 import com.organiza.api.modules.transactions.infra.database.repository.TransactionRepository;
+import com.organiza.api.modules.transactions.processor.TransactionPaymentProcessor;
+import com.organiza.api.modules.users.infra.database.entity.UserModel;
 import com.organiza.api.modules.users.infra.database.repository.UserRepository;
 import com.organiza.api.shared.common.utils.consts.TransactionsErrorConsts;
 import com.organiza.api.shared.common.utils.consts.UserErrorConsts;
@@ -17,10 +20,12 @@ public class DeleteTransactionService {
 
     private final UserRepository userRepository;
     private final TransactionRepository transactionRepository;
+    private final BudgetRepository budgetRepository;
+    private final TransactionPaymentProcessor paymentProcessor;
 
     public void execute(String user_id, String id){
 
-        userRepository.findById(UUID.fromString(user_id)).orElseThrow(
+      UserModel user = userRepository.findById(UUID.fromString(user_id)).orElseThrow(
                 () -> new HttpNotFoundException(
                         UserErrorConsts.USER_NOT_FOUND)
         );
@@ -30,6 +35,14 @@ public class DeleteTransactionService {
                 new HttpNotFoundException(TransactionsErrorConsts.TRANSACTION_NOT_FOUND)
 
         );
+
+        if (transaction.getType().toString().equals("DESPESA")) {
+            TransactionModel transactionWithCreatedAt = transactionRepository.save(transaction);
+
+            paymentProcessor.processInvestmentOfABudgetExistentDelete(transactionWithCreatedAt, user);
+
+            return;
+        }
 
     transactionRepository.delete(transaction);
 
