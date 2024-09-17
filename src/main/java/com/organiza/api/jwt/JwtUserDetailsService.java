7061d714aa702e6239433754b55b216a786c193d
EmanuelErnesto <emanuel.earego@gmail.com>
2024-09-17 02:04:05 -0300

    fix: exception format message

--- src/main/java/com/organiza/api/jwt/JwtUserDetailsService.java
+++ src/main/java/com/organiza/api/jwt/JwtUserDetailsService.java
@@ -5,13 +5,17 @@ import com.organiza.api.modules.users.infra.database.repository.UserRepository;
 import com.organiza.api.shared.common.utils.consts.UserErrorConsts;
 import com.organiza.api.exception.HttpNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class JwtUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
@@ -19,7 +23,7 @@ public class JwtUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         UserModel user = userRepository.findByEmail(email).orElseThrow(
-                () -> new HttpNotFoundException(String.format("user with email 'email' not found", email))
+                () -> new HttpNotFoundException(String.format("user with email '%s' not found", email))
         );
 
         return new JwtUserDetails(user);
@@ -30,7 +34,10 @@ public class JwtUserDetailsService implements UserDetailsService {
         UserModel.Role role = userRepository.findRoleByEmail(email).orElseThrow(() ->
                 new HttpNotFoundException(UserErrorConsts.USER_NOT_FOUND)
         );
+        UUID userId = userRepository.findUserIdByEmail(email).orElseThrow(
+                () -> new HttpNotFoundException(UserErrorConsts.USER_NOT_FOUND)
+        );
 
-        return JwtUtils.createToken(email, role.name().substring("ROLE_".length()));
+        return JwtUtils.createToken(email, userId ,role.name().substring("ROLE_".length()));
     }
 }
