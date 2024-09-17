19271f64494309079267fbd3d9e9c0fa0b3b7b93
EmanuelErnesto <emanuel.earego@gmail.com>
2024-09-17 02:09:35 -0300

    raw: change env config

--- src/main/java/com/organiza/api/modules/transactions/domain/dtos/mappers/TransactionMapper.java
+++ src/main/java/com/organiza/api/modules/transactions/domain/dtos/mappers/TransactionMapper.java
@@ -1,70 +1,59 @@
-package com.organiza.api.modules.transactions.domain.dtos.mappers;
+    package com.organiza.api.modules.transactions.domain.dtos.mappers;
 
-import com.organiza.api.modules.transactions.domain.dtos.CreateTransactionDto;
-import com.organiza.api.modules.transactions.domain.dtos.TransactionResponseDto;
-import com.organiza.api.modules.transactions.domain.dtos.UpdateTransactionDto;
-import com.organiza.api.modules.transactions.infra.database.entity.TransactionModel;
-import com.organiza.api.modules.users.infra.database.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
-import org.modelmapper.PropertyMap;
-import org.springframework.stereotype.Component;
+    import com.organiza.api.modules.transactions.domain.dtos.CreateTransactionDto;
+    import com.organiza.api.modules.transactions.domain.dtos.TransactionResponseDto;
+    import com.organiza.api.modules.transactions.infra.database.entity.TransactionModel;
+    import com.organiza.api.modules.users.infra.database.repository.UserRepository;
+    import lombok.RequiredArgsConstructor;
+    import org.modelmapper.ModelMapper;
+    import org.modelmapper.PropertyMap;
+    import org.springframework.stereotype.Component;
 
-import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
+    import java.time.format.DateTimeFormatter;
+    import java.util.List;
+    import java.util.UUID;
+    import java.util.stream.Collectors;
 
-@RequiredArgsConstructor
-@Component
-public class TransactionMapper {
-
-
-    private final UserRepository userRepository;
+    @RequiredArgsConstructor
+    @Component
+    public class TransactionMapper {
 
 
-    public TransactionModel mappingToTransactionModel(CreateTransactionDto createTransactionDto) {
-        TransactionModel transactionModel = new ModelMapper().map(createTransactionDto, TransactionModel.class);
-
-        userRepository.findById(UUID.fromString(createTransactionDto.getUser_id())).ifPresent(transactionModel::setUser);
-
-        return transactionModel;
-    }
-
-    public static TransactionResponseDto mappingToTransactionResponse(TransactionModel transaction) {
-        String type = transaction.getType().toString().toLowerCase();
-        String status = transaction.getStatus().toString().toLowerCase();
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        String date = transaction.getDate_payment().format(formatter);
-
-        PropertyMap<TransactionModel, TransactionResponseDto> props = new PropertyMap<TransactionModel, TransactionResponseDto>() {
-            @Override
-            protected void configure() {
-                map().setType(type);
-                map().setStatus(status);
-                map().setDate_payment(date);
-            }
-        };
-        ModelMapper mapper = new ModelMapper();
-        mapper.addMappings(props);
-
-        return mapper.map(transaction, TransactionResponseDto.class);
-    }
-
-    public static List<TransactionResponseDto> mappingToTransactionListResponse(List<TransactionModel> transactions){
-        return transactions.stream().map(TransactionMapper::mappingToTransactionResponse).collect(Collectors.toList());
-    }
-
-    public TransactionModel mappingUpdateTransactionDtoToTransactionModel(UpdateTransactionDto updateTransactionDto) {
-
-        TransactionModel transactionModel = new ModelMapper().map(updateTransactionDto, TransactionModel.class);
+        private final UserRepository userRepository;
 
 
-        userRepository.findById(UUID.fromString(updateTransactionDto.getUser_id())).ifPresent(transactionModel::setUser);
+        public TransactionModel mappingToTransactionModel(CreateTransactionDto createTransactionDto) {
+            TransactionModel transactionModel = new ModelMapper().map(createTransactionDto, TransactionModel.class);
 
-        return transactionModel;
+            userRepository.findById(UUID.fromString(createTransactionDto.getUser_id())).ifPresent(transactionModel::setUser);
+
+            return transactionModel;
+        }
+
+        public static TransactionResponseDto mappingToTransactionResponse(TransactionModel transaction) {
+            String type = transaction.getType().toString().toLowerCase();
+            String status = transaction.getStatus().toString().toLowerCase();
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            String date = transaction.getDate_payment().format(formatter);
+
+            PropertyMap<TransactionModel, TransactionResponseDto> props = new PropertyMap<TransactionModel, TransactionResponseDto>() {
+                @Override
+                protected void configure() {
+                    map().setType(type);
+                    map().setStatus(status);
+                    map().setDate_payment(date);
+                }
+            };
+            ModelMapper mapper = new ModelMapper();
+            mapper.addMappings(props);
+
+            return mapper.map(transaction, TransactionResponseDto.class);
+        }
+
+        public static List<TransactionResponseDto> mappingToTransactionListResponse(List<TransactionModel> transactions){
+            return transactions.stream().map(TransactionMapper::mappingToTransactionResponse).collect(Collectors.toList());
+        }
+
 
     }
-
-}
