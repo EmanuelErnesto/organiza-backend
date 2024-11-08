86733b84b3d1bc05d1d2896653da00a5fbe03553
kawanlb <kawandev7@gmail.com>
2024-11-07 22:24:14 -0300

    adding localhost to AllowedOrigins

--- src/main/java/com/organiza/api/config/SpringSecurityConfig.java
+++ src/main/java/com/organiza/api/config/SpringSecurityConfig.java
@@ -75,7 +75,7 @@ public class SpringSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://organizav1.vercel.app"));
+        configuration.setAllowedOrigins(List.of("https://organizav1.vercel.app", "http://localhost:3000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
